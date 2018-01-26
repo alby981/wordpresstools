daf50311d819b5ad9d415f7ffd4cda35d60f8d69
Alberto Belotti <albertopuntobelotti@gmail.com>
2018-01-26 15:44:03 +0100

    Update insertPost.php
    Small refactoring... needs to be tested.

--- insertPost.php
+++ insertPost.php
@@ -1,13 +1,32 @@
 <?php
 require('./wp-load.php');
+
+//TODO: hardcoded? mmmm
 $author_id = 1;
-//array with parent ID page
+//TODO: needs to get value from wp database. 
 $aParent = ["telefonia" => 15,"aerei" => 22,"auto"=>19,"moda"=>735];
-//use file?
-$file = file("auto.txt");
-//use array?
-$file = ['Chanel','Dior','Dolce & Gabbana','Gucci','Versace','Yves Saint Laurent','Armani','Louis Vuitton','Moschino','Fiorucci','Lacoste','Valentino'];
-foreach ($file as $title) {
+$aSettings['source'] = 'file';
+$aSettings['parent'] = 'moda';
+$aSources = ['file','array'];
+
+//TODO: filename 
+$filename = "auto.txt";
+if (!in_array($aSettings['source'],$aSources)) {
+	die("set data source");
+}
+if (!in_array($aSettings['parent'],$aParent)) {
+	die("set correct parent");
+}
+switch ($aSettings['source']) {
+	case 'file':
+		$data = file($filename);
+		break;
+	case 'array':
+		$data = ['Chanel','Dior','Dolce & Gabbana','Gucci','Versace','Yves Saint Laurent','Armani','Louis Vuitton','Moschino','Fiorucci','Lacoste','Valentino'];
+		break;
+}
+
+foreach ($data as $title) {
 	$slug = sanitize_title($title);
 	if(empty($slug))
 		continue;
@@ -16,10 +35,10 @@ foreach ($file as $title) {
 			'comment_status'	=>	'closed',
 			'ping_status'		=>	'closed',
 			'post_author'		=>	$author_id,
-			'post_name'		    =>	$slug,
+			'post_name'		=>	$slug,
 			'post_title'		=>	$title,
 			'post_status'		=>	'publish',
-			'post_type'		    =>	'page',
+			'post_type'		=>	'page',
 			'post_parent'		=>	$aParent["moda"]
 		]
 	);
